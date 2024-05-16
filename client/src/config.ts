2c4d8a282aa68e290ade549cc8390a15205906f6
Edward Lukman <edwlaw12@gmail.com>
2024-05-16 21:32:54 +1000

    Integrating google oauth

---
+++ client/src/config.ts
@@ -0,0 +1,6 @@
+const config = {
+  googleOAuthClientId:
+    "90546165250-veqe1gvk3v0lhv6r03agju7p4nq1tcee.apps.googleusercontent.com",
+};
+
+export default config;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import config from "./config";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <GoogleOAuthProvider clientId={config.googleOAuthClientId}>
+      <App />
+    </GoogleOAuthProvider>
+  </React.StrictMode>
+);
