a1f90fba557bdbd223ecdb9a4032cfa7aea25639
Hellen Wang <hellenyilin23@gmail.com>
2024-05-21 22:32:30 +1000

    changed controllers and routes to simpler format

--- server/src/contollers/user.controller.ts
+++ server/src/contollers/user.controller.ts
@@ -2,37 +2,56 @@ import UserModel from "../mongodb/models/user";
 import { Request, Response } from "express";
 
 // creates a user if the id doesnt exist already
-export const userCreate = async (email: string) => {
-    const userFound = await UserModel.findOne({email});
+export const userCreate = async (req: Request, res: Response) => {
+    
+    try {
+        const email = req.body;
+        const userFound = await UserModel.findOne({email});
 
-    if (userFound) throw new Error("User already exists!");
+        if (userFound) res.status(200).json(userFound._id)
 
-    const newUser = await UserModel.create({
-        email
-    })
+        const newUser = await UserModel.create({
+            email
+        })
 
-    return newUser._id;
+        res.status(200).json(newUser._id)
+    } catch (error) {
+        res.status(500).json({message: "User already exists"});
+    }
+
 }
 
-
 // delete user
-export const userDelete = async (email: string) => {
-    const userFound = await UserModel.findOne({email});
+export const userDelete = async (req: Request, res: Response) => {
+    
+    try {
+        const email = req.body;
+        const userFound = await UserModel.findOne({email});
 
-    if (!userFound) throw new Error("User not found!");
+        if (!userFound) throw new Error("User not found!");
 
-    await UserModel.deleteOne({email});
+        await UserModel.deleteOne({email});
 
-    return {}
+        res.status(200).json({})
+    } catch (error) {
+        res.status(500).json({message: "User doesnt exists"});
+    }
 
 }
 
 // get user email 
-export const userGet = async (id: number) => {
-    const userFound = await UserModel.findOne({_id: id}).populate("allProperties");
+export const userGet = async (req: Request, res: Response) => {
 
-    if (!userFound) throw new Error("User not found!");
+    try {
+        const id = parseInt(req.params.id);
 
-    return userFound.email;
+        const userFound = await UserModel.findOne({_id: id}).populate("allProperties");
+
+        if (!userFound) throw new Error("User not found!");
+
+        res.status(200).json(userFound.email)
+    } catch (error) {
+        res.status(500).json({message: "User doesnt exists"});
+    }
 
 }
--- server/src/routes/user.route.ts
+++ server/src/routes/user.route.ts
@@ -1,44 +1,10 @@
-import { Router, Request, Response } from 'express';
+import { Router } from 'express';
 import { userCreate, userDelete, userGet } from '../contollers/user.controller'
 
 const router = Router();
 
-router.post('/user', (req: Request, res: Response) => {
-    const email = req.body;
+router.post('/create/v1', userCreate);
 
-    try {
-        const userId = userCreate(email);
+router.delete('/delete/v1', userDelete);
 
-        res.status(200).json(userId)
-    } catch (error) {
-        res.status(500).json({message: "User already exists"});
-    }
-
-});
-
-router.delete('/user', (req: Request, res: Response) => {
-    const email = req.body;
-
-    try {
-        const userId = userDelete(email);
-
-        res.status(200).json(userId)
-    } catch (error) {
-        res.status(500).json({message: "User doesnt exists"});
-    }
-
-});
-
-
-router.get('/user/:id', (req: Request, res: Response) => {
-    const id = parseInt(req.params.id);
-
-    try {
-        const userId = userGet(id);
-
-        res.status(200).json(userId)
-    } catch (error) {
-        res.status(500).json({message: "User doesnt exists"});
-    }
-
-});
+router.get('/get/:id', userGet);
