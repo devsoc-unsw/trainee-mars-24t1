d9fdf07cf66a9a60ea80f7995aa08217a9832f8d
Jessica O'Brien <z5479430@ad.unsw.edu.au>
2024-05-22 18:36:44 +1000

    adding userId to prompt/answer routes and updating error checks to match

--- server/src/controllers/answer.controller.ts
+++ server/src/controllers/answer.controller.ts
@@ -1,11 +1,17 @@
 import { Request, Response } from 'express';
 import PromptModel from "../mongodb/models/prompt";
+import UserModel from "../mongodb/models/user";
+import { userHasPromptById } from './prompt.controller';
 
 export const answerReplaceText = async (req: Request, res: Response) => {
-    const { promptId, newText } = req.body;
+    const { userId, promptId, newText } = req.body;
     try {
-        const existingPrompt = await PromptModel.findOne({ "_id": promptId });
-        if (!existingPrompt) {
+        const user = await UserModel.findById(userId);
+        if (!user) {
+            res.status(400).json({ message: "User does not exist" });
+            return;
+        }
+        if (!(await userHasPromptById(userId, promptId))) {
             res.status(400).json({ message: "Prompt does not exist" });
             return;
         }
