7ac272c787b5de27110718aef4836492556246fe
Aiden Rawles <aidenrawles27@gmail.com>
2024-05-23 19:02:12 +1000

    fixed formatting of user create output

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -14,9 +14,9 @@ export const userCreate = async (req: Request, res: Response) => {
             email
         })
 
-        res.status(200).json(newUser._id)
+        res.status(200).json({ userId: newUser._id })
     } catch (error) {
-        res.status(500).json({message: "unknown"});
+        res.status(500).json({ message: "unknown" });
     }
 
 }
@@ -26,15 +26,15 @@ export const userDelete = async (req: Request, res: Response) => {
     const { email } = req.body;
     
     try {
-        const userFound = await UserModel.findOne({"email": email});
+        const userFound = await UserModel.findOne({ "email": email });
 
         if (!userFound) throw new Error("User not found!");
 
-        await UserModel.deleteOne({email});
+        await UserModel.deleteOne({ email });
 
         res.status(200).json({})
     } catch (error) {
-        res.status(500).json({message: "User doesnt exists"});
+        res.status(500).json({ message: "User doesnt exists" });
     }
 
 }
@@ -45,13 +45,13 @@ export const userGet = async (req: Request, res: Response) => {
     try {
         const id = parseInt(req.params.id);
 
-        const userFound = await UserModel.findOne({"_id": id}).populate("allProperties");
+        const userFound = await UserModel.findOne({ "_id": id }).populate("allProperties");
 
         if (!userFound) throw new Error("User not found!");
 
-        res.status(200).json(userFound.email)
+        res.status(200).json({ email: userFound.email })
     } catch (error) {
-        res.status(500).json({message: "User doesnt exists"});
+        res.status(500).json({ message: "User doesnt exists" });
     }
 
 }
